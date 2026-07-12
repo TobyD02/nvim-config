40313c4cc0c4247d0c49b65b2844e90b9533189e
Toby Dixon <thetjdixon1234@gmail.com>
2026-07-12 21:57:04 +0100

    only spellcheck on certain files

--- common.lua
+++ common.lua
@@ -33,5 +33,10 @@ vim.filetype.add({
   },
 })
 
-vim.opt.spell = true
-vim.opt.spelllang = {"en_gb"}
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "text" },
+    callback = function()
+        vim.opt_local.spell = true
+        vim.opt_local.spelllang = { "en_gb" }
+    end,
+})
